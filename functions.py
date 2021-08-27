80537a64c8c54d66ef7f8c92a96c0b69ff75c854
vladoskondra <43408117+vladoskondra@users.noreply.github.com>
2021-08-27 23:32:16 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -27,6 +27,7 @@ app = Client(
     SESSION_STRING if HEROKU else "tgvc",
     api_id=API_ID,
     api_hash=API_HASH,
+    test_mode=True,
     phone_number="+79164489750"
 )
 session = ClientSession()
