04bc84b2709856940b770248f660af0af2414f42
vladoskondra <43408117+vladoskondra@users.noreply.github.com>
2021-08-27 22:37:40 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,18 +1,18 @@
-HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+HELP_TEXT = """__**Я устроился на работу диджеем в этот чат и вот че я могу:**__
 
-**/skip** __Skip The Current Playing Music.__
-**/play** __Service_Or_Default (Services: youtube/saavn, Default: youtube) Song_Name | Reply_To_Audio__
-**/joinvc** __Join Voice Chat.__
-**/leavevc** __Leave Voice Chat.__
-**/volume [1-200]** __Adjust Volume.__
-**/pause** __Pause Music.__
-**/resume** __Resume Music.__
-**/queue plformat/Nothing** __Shows Queue List. If you send plformat with command you will get it in playlist format.__
-**/delqueue** __Deletes Queue List and Playlist.__
-**/playlist** __Start Playing Playlist.__
+**/skip** __Пропустить играющую ща музыку__
+**/play** __Сыграть вашу музыку по заявке ('/play Название песни' или '/play' ответом на файл)__
+**/dj_vlados_join** __Пригласить меня, DJ Vlados, за диджейский пульт__
+**/dj_vlados_leave** __Закончить мою смену и выплатить гонорар__
+**/volume [1-200]** __Подкрутить громкость__
+**/pause** __Приостановить музыку__
+**/resume** __Продолжить играть музыку__
+**/queue** __Показать список заявок__
+**/delqueue** __Очистить список заявок__
+**/playlist** __Создать плейлист__
 """
 
 REPO_TEXT = (
-    "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
-    + " | [Group](t.me/TGVCSUPPORT)"
+    "хуй"
+    + " хуй"
 )
